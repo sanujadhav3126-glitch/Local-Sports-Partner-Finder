31bf90c4d7c585ca643c08da83076c4327b7ff26
sanujadhav3126-glitch <sanujadhav3126@gmail.com>
2026-06-04 19:16:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:5173','http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['https://local-sports-partner-finder.vercel.app','http://localhost:3000'], credentials: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Local Sports Partner Finder API running'));
